fd2849cda43fcba6af1c7a703cda661232c6bcc9
Brandon Fong <bfongbfong@gmail.com>
2019-11-29 15:43:52 -0500

    Conforms SavedIngredientVC to order with MARK

--- WhatToEat/Controller/SavedIngredientViewController.swift
+++ WhatToEat/Controller/SavedIngredientViewController.swift
@@ -89,33 +89,6 @@ extension SavedIngredientViewController {
 // MARK: - UI Functions
 extension SavedIngredientViewController {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return savedIngredients.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = savedIngredientTableView.dequeueReusableCell(withIdentifier: "SavedIngredientCell") as! SavedIngredientTableViewCell
-        cell.updateCell(with: savedIngredients[indexPath.row])
-        cell.selectionStyle = .gray
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            savedIngredients.remove(at: indexPath.row)
-            savedIngredientTableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-    
     func setEditing(enabled: Bool) {
         if enabled {
             self.navigationController?.setToolbarHidden(false, animated: true)
@@ -130,17 +103,47 @@ extension SavedIngredientViewController {
         }
     }
     
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+// MARK: - UITableView Data Source
+extension SavedIngredientViewController {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return savedIngredients.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = savedIngredientTableView.dequeueReusableCell(withIdentifier: "SavedIngredientCell") as! SavedIngredientTableViewCell
+        cell.updateCell(with: savedIngredients[indexPath.row])
+        cell.selectionStyle = .gray
+        return cell
+    }
+    
+}
+
+// MARK: - UITableView Delegate
+extension SavedIngredientViewController {
+    
+    func tableView(_ tableView: UITableView, shouldHighlightRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            savedIngredients.remove(at: indexPath.row)
+            savedIngredientTableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
 }
 
-// update saved items in view controller
+// MARK: - UINavigationControllerDelegate
 extension SavedIngredientViewController: UINavigationControllerDelegate {
+    
+    // update saved items in view controller
     func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
         setEditing(enabled: false)
         if let homeViewController = (viewController as? HomeViewController) {
