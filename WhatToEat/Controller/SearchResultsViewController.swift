d7bc5e5c85bb99260be32d7f00893330bc67ba62
Brandon Fong <bfongbfong@gmail.com>
2019-11-30 16:01:24 -0500

    Modularizes API call to SpoonacularManager

--- WhatToEat/Controller/SearchResultsViewController.swift
+++ WhatToEat/Controller/SearchResultsViewController.swift
@@ -201,10 +201,8 @@ extension SearchResultsViewController {
                        originalString: missedIngredientsArray[i]["originalString"] as! String,
                        unit: missedIngredientsArray[i]["unit"] as! String,
                        unitShort: missedIngredientsArray[i]["unitShort"] as! String)
-                   //                                    print("adding missing ingredient called \(ingredient.name)")
                    
                    recipe.missedIngredients.append(ingredient)
-                   //                                    print("missed ingredient just added called: \(recipe.missedIngredients[i].name)")
                }
            }
            if let unusedIngredientsArray = dictionary["unusedIngredients"] as? [[String: Any]] {
@@ -236,9 +234,7 @@ extension SearchResultsViewController {
                        unit: usedIngredientsArray[i]["unit"] as! String,
                        unitShort: usedIngredientsArray[i]["unitShort"] as! String)
                    
-                   //                                    print("adding used ingredient called \(ingredient.name)")
                    recipe.usedIngredients.append(ingredient)
-                   //                                    print("used ingredient just added called: \(recipe.usedIngredients[i].name)")
                }
            }
            
@@ -246,118 +242,7 @@ extension SearchResultsViewController {
        }
        
         completion()
-//       DispatchQueue.main.async {
-//           self.tableView.reloadData()
-//       }
     }
-
-    func getRecipes(ingredients: String, numberOfResults: Int, ignorePantry: Bool) {
-        UNIRest.get { (request) in
-            
-            let requestString: String = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=\(numberOfResults)&ranking=1&ignorePantry=\(String(ignorePantry))&ingredients=\(ingredients)"
-            
-            print(requestString)
-            print("==============================================================")
-            print("RECIPES")
-            
-            guard let unwrappedRequest = request else { return }
-            
-            unwrappedRequest.url = requestString
-            unwrappedRequest.headers = ["X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com", "X-RapidAPI-Key": "ba59075c47msh50cd1afad35f3adp1d65cdjsn4b0f3c045f70"]
-            
-            }?.asJsonAsync({ (response, error) in
-                
-                guard let response = response else { return }
-                guard let body: UNIJsonNode = response.body else { return }
-                guard let bodyJsonArray = body.jsonArray() else { return }
-                
-//                // to prevent duplicate recipes
-//                var setOfIDs: Set<Int> = []
-//                var setOfTitles: Set<String> = []
-//                print("JSON ARRAY ==================================================")
-//                print(bodyJsonArray)
-//                for json in bodyJsonArray {
-//                    let recipe = Recipe()
-//                    guard let dictionary = json as? [String : Any] else { return }
-//                    let id = dictionary["id"] as? Int
-//                    if id != nil && !setOfIDs.contains(id!) {
-//                        setOfIDs.insert(id!)
-//                        recipe.id = id
-//                    } else { continue }
-//                    guard let title = dictionary["title"] as? String else { continue }
-//                    if !setOfTitles.contains(title) {
-//                        setOfTitles.insert(title)
-//                        recipe.title = dictionary["title"] as? String
-//                    } else { continue }
-//                    recipe.imageName = dictionary["image"] as? String
-//                    recipe.missedIngredientCount = dictionary["missedIngredientCount"] as? Int
-//                    recipe.usedIngredientCount = dictionary["usedIngredientCount"] as? Int
-//                    recipe.unusedIngredientCount = dictionary["unusedIngredientCount"] as? Int
-//                    if let missedIngredientsArray = dictionary["missedIngredients"] as? [[String : Any]]  {
-//                        for i in 0..<missedIngredientsArray.count {
-//
-//                            let ingredient = Ingredient(
-//                                aisle: missedIngredientsArray[i]["aisle"] as? String ?? "",
-//                                amount: missedIngredientsArray[i]["amount"] as! NSNumber,
-//                                id: missedIngredientsArray[i]["id"] as! Int,
-//                                imageName: missedIngredientsArray[i]["imageName"] as? String ?? "no image name",
-//                                name: missedIngredientsArray[i]["name"] as! String,
-//                                originalString: missedIngredientsArray[i]["originalString"] as! String,
-//                                unit: missedIngredientsArray[i]["unit"] as! String,
-//                                unitShort: missedIngredientsArray[i]["unitShort"] as! String)
-//                            //                                    print("adding missing ingredient called \(ingredient.name)")
-//
-//                            recipe.missedIngredients.append(ingredient)
-//                            //                                    print("missed ingredient just added called: \(recipe.missedIngredients[i].name)")
-//                        }
-//                    }
-//                    if let unusedIngredientsArray = dictionary["unusedIngredients"] as? [[String: Any]] {
-//                        for i in 0..<unusedIngredientsArray.count {
-//
-//                            let ingredient = Ingredient(
-//                                aisle: unusedIngredientsArray[i]["aisle"] as? String ?? "",
-//                                amount: unusedIngredientsArray[i]["amount"] as! NSNumber,
-//                                id: unusedIngredientsArray[i]["id"] as! Int,
-//                                imageName: unusedIngredientsArray[i]["imageName"] as? String ?? "no image name",
-//                                name: unusedIngredientsArray[i]["name"] as! String,
-//                                originalString: unusedIngredientsArray[i]["originalString"] as! String,
-//                                unit: unusedIngredientsArray[i]["unit"] as! String,
-//                                unitShort: unusedIngredientsArray[i]["unitShort"] as! String)
-//
-//                            recipe.unusedIngredients.append(ingredient)
-//                        }
-//                    }
-//                    if let usedIngredientsArray = dictionary["usedIngredients"] as? [[String: Any]] {
-//                        for i in 0..<usedIngredientsArray.count {
-//
-//                            let ingredient = Ingredient(
-//                                aisle: usedIngredientsArray[i]["aisle"] as! String,
-//                                amount: usedIngredientsArray[i]["amount"] as! NSNumber,
-//                                id: usedIngredientsArray[i]["id"] as! Int,
-//                                imageName: usedIngredientsArray[i]["imageName"] as? String ?? "no image name",
-//                                name: usedIngredientsArray[i]["name"] as! String,
-//                                originalString: usedIngredientsArray[i]["originalString"] as! String,
-//                                unit: usedIngredientsArray[i]["unit"] as! String,
-//                                unitShort: usedIngredientsArray[i]["unitShort"] as! String)
-//
-//                            //                                    print("adding used ingredient called \(ingredient.name)")
-//                            recipe.usedIngredients.append(ingredient)
-//                            //                                    print("used ingredient just added called: \(recipe.usedIngredients[i].name)")
-//                        }
-//                    }
-//
-//                    self.recipes.append(recipe)
-//                }
-//
-//                DispatchQueue.main.async {
-//                    self.tableView.reloadData()
-//                }
-                
-            })
-    }
-    
-    
-    
 
     
     
