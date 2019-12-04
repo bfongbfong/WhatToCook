7f961832228220ebaa041f0536b339aab4280784
Brandon Fong <bfongbfong@gmail.com>
2019-12-03 23:58:52 -0500

    Moves downloadImage to NetworkRequests

--- WhatToEat/View/SearchResultTableViewCell.swift
+++ WhatToEat/View/SearchResultTableViewCell.swift
@@ -110,27 +110,14 @@ class SearchResultTableViewCell: UITableViewCell {
     func addImage(recipe: Recipe) {
         if let unwrappedRecipeImageName = recipe.imageName{
             if let url = URL(string: unwrappedRecipeImageName) {
-                downloadImage(from: url)
+                NetworkRequests.downloadImage(from: url) { (data) in
+                    DispatchQueue.main.async() {
+                        self.imageLabel.image = UIImage(data: data)
+                    }
+                }
             }
         }
     }
-    
-    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
-        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
-    }
-    
-    func downloadImage(from url: URL) {
-        print("Download Started")
-        getData(from: url) { data, response, error in
-            guard let data = data, error == nil else { return }
-            print(response?.suggestedFilename ?? url.lastPathComponent)
-            print("Download Finished")
-            DispatchQueue.main.async() {
-                self.imageLabel.image = UIImage(data: data)
-            }
-        }
-    }
-
 }
 
 
