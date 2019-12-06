9ba2cd277317368707881f7ad25bc2ae1cfd9a06
Brandon Fong <bfongbfong@gmail.com>
2019-12-06 14:04:16 -0500

    Replaces api call with singleton one in SpoonacularManager and separates the API call with the json parsing

--- WhatToEat/Controller/SavedRecipesViewController.swift
+++ WhatToEat/Controller/SavedRecipesViewController.swift
@@ -95,15 +95,14 @@ class SavedRecipesViewController: UIViewController, UITableViewDelegate, UITable
             if savedRecipes.count != 0 {
                 let difference = PersistenceManager.bookmarkedRecipeIDs.count - savedRecipes.count
                 for i in 0..<difference {
-                    getRecipeFromIntAndAddToSavedRecipesArray(id: PersistenceManager.bookmarkedRecipeIDs[savedRecipes.count + i])
+                    populateRecipeData(id: PersistenceManager.bookmarkedRecipeIDs[savedRecipes.count + i])
                 }
 
             } else {
                 if savedRecipes.count == 0 {
                     
                     for recipeID in PersistenceManager.bookmarkedRecipeIDs {
-                        
-                        getRecipeFromIntAndAddToSavedRecipesArray(id: recipeID)
+                        populateRecipeData(id: recipeID)
                         print("SAVED RECIPE VC VIEW DID LOAD HAPPENED")
                     }
                 }
@@ -174,68 +173,63 @@ class SavedRecipesViewController: UIViewController, UITableViewDelegate, UITable
         savedRecipesTableView.reloadData()
     }
     
-    func getRecipeFromIntAndAddToSavedRecipesArray(id: Int) {
+    func populateRecipeData(id: Int) {
+        SpoonacularManager.getRecipeInformation(recipeId: id) { (json, error) in
+            if let errorThatHappened = error {
+                print(errorThatHappened.localizedDescription)
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.parseJson(json: json, id: id)
+                
+                if self.savedRecipes.count > 1 && PersistenceManager.bookmarkedRecipeIDs.count == self.savedRecipes.count {
+                    self.reorderSavedRecipesArray()
+                }
+                self.savedRecipesTableView.reloadData()
+            }
+        }
+    }
+    
+    func parseJson(json: [String: Any]?, id: Int) {
         
         let returnRecipe = Recipe()
         
-        UNIRest.get { (request) in
+        if let bodyJsonObject = json {
+            print("JSON OBJECT ==================================================")
+            print(bodyJsonObject)
             
-            let requestString = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/\(id)/information"
+            returnRecipe.source = bodyJsonObject["sourceUrl"] as? String
+            returnRecipe.imageName = bodyJsonObject["image"] as? String
+            returnRecipe.servings = bodyJsonObject["servings"] as? Int
+            returnRecipe.readyInMinutes = bodyJsonObject["readyInMinutes"] as? Int
+            returnRecipe.diets = bodyJsonObject["diets"] as? [String]
+            returnRecipe.title = bodyJsonObject["title"] as? String
+            returnRecipe.creditsText = bodyJsonObject["creditsText"] as? String
             
-            if let unwrappedRequest = request {
-                unwrappedRequest.url = requestString
-                unwrappedRequest.headers = ["X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com", "X-RapidAPI-Key": "ba59075c47msh50cd1afad35f3adp1d65cdjsn4b0f3c045f70"]
-            }
-            
-            }?.asJsonAsync({ (response, error) in
-                
-                if let response = response {
-                    if let body: UNIJsonNode = response.body {
-                        if let bodyJsonObject = body.jsonObject() {
-                            print("JSON OBJECT ==================================================")
-                            print(bodyJsonObject)
-                            
-                            returnRecipe.source = bodyJsonObject["sourceUrl"] as? String
-                            returnRecipe.imageName = bodyJsonObject["image"] as? String
-                            returnRecipe.servings = bodyJsonObject["servings"] as? Int
-                            returnRecipe.readyInMinutes = bodyJsonObject["readyInMinutes"] as? Int
-                            returnRecipe.diets = bodyJsonObject["diets"] as? [String]
-                            returnRecipe.title = bodyJsonObject["title"] as? String
-                            returnRecipe.creditsText = bodyJsonObject["creditsText"] as? String
-                            
-                            if let ingredientsArray = bodyJsonObject["extendedIngredients"] as? [[String:Any]] {
-                                for ingredient in ingredientsArray {
-                                    let ingredientName = ingredient["name"] as? String
-                                    let ingredientAmount = ingredient["amount"] as? Int
-                                    let ingredientAisle = ingredient["aisle"] as? String
-                                    let ingredientUnit = ingredient["unit"] as? String
-                                    let ingredientId = ingredient["id"] as? Int
-                                    let ingredientImage = ingredient["image"] as? String
-                                    var ingredientUnitShort: String?
-                                    
-                                    if let measures = ingredient["measures"] as? [String: Any] {
-                                        if let us = measures["us"] as? [String: Any] {
-                                            ingredientUnitShort = us["unitShort"] as? String
-                                        }
-                                    }
-                                    
-                                    let newIngredient = Ingredient(aisle: ingredientAisle, amount: ingredientAmount as NSNumber?, id: ingredientId, imageName: ingredientImage, name: ingredientName, unit: ingredientUnit, unitShort: ingredientUnitShort)
-                                    returnRecipe.ingredients.append(newIngredient)
-                                }
-                            }
-                            returnRecipe.id = id
-                            self.savedRecipes.append(returnRecipe)
-                        }
-                        
-                        DispatchQueue.main.async {
-                            if self.savedRecipes.count > 1 && PersistenceManager.bookmarkedRecipeIDs.count == self.savedRecipes.count {
-                                self.reorderSavedRecipesArray()
-                            }
-                            self.savedRecipesTableView.reloadData()
+            if let ingredientsArray = bodyJsonObject["extendedIngredients"] as? [[String:Any]] {
+                for ingredient in ingredientsArray {
+                    let ingredientName = ingredient["name"] as? String
+                    let ingredientAmount = ingredient["amount"] as? Int
+                    let ingredientAisle = ingredient["aisle"] as? String
+                    let ingredientUnit = ingredient["unit"] as? String
+                    let ingredientId = ingredient["id"] as? Int
+                    let ingredientImage = ingredient["image"] as? String
+                    var ingredientUnitShort: String?
+                    
+                    if let measures = ingredient["measures"] as? [String: Any] {
+                        if let us = measures["us"] as? [String: Any] {
+                            ingredientUnitShort = us["unitShort"] as? String
                         }
                     }
+                    
+                    let newIngredient = Ingredient(aisle: ingredientAisle, amount: ingredientAmount as NSNumber?, id: ingredientId, imageName: ingredientImage, name: ingredientName, unit: ingredientUnit, unitShort: ingredientUnitShort)
+                    returnRecipe.ingredients.append(newIngredient)
                 }
-            })
+            }
+            returnRecipe.id = id
+            self.savedRecipes.append(returnRecipe)
+        }
     }
     
 
